cbf2877acb3c4a181f55c9e54c79ff14b65b6b80
HCHBCORP\cmanning <cmanning@hchb.com>
2022-08-29 10:06:40 -0500

    added wait for person search page load and basic test to web

--- pageobjects/person_search.page.js
+++ pageobjects/person_search.page.js
@@ -2,6 +2,14 @@ const Page = require("./page");
 
 class Person_searchPage extends Page {
 
+    get searchField() {
+        return $('//input[@class="k-input-inner"]');
+    }
+
+    async onPersonSearchPage() {
+        await this.searchField.waitForDisplayed({ timeout: 10000 });
+    }
+
 }
 
 module.exports = new Person_searchPage();
--- test/web/mustang.web.test.js
+++ test/web/mustang.web.test.js
@@ -1,16 +1,18 @@
-const MustangPage = require("../../pageobjects/main.page");
+const MainPage = require("../../pageobjects/main.page");
+const PerssonSearchPage = require("../../pageobjects/person_search.page")
 
-describe("Mustang POC", () => {
+describe("Mustang Web Tests", () => {
 
     beforeAll(async function () {
         browser.url('/')
     });
 
     it("can click calendar icon", async () => {
-        await MustangPage.clickCalendar(true);
+        await MainPage.clickCalendar(true);
     });
 
     it("can click person search icon", async () => {
-        await MustangPage.clickPersonSearch();
+        await MainPage.clickPersonSearch();
+        await PerssonSearchPage.onPersonSearchPage()
     });
 });
