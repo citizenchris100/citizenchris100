50a9b9e15ab36110f0f1dff85db7c49059684b07
HCHBCORP\cmanning <cmanning@hchb.com>
2022-08-29 09:21:11 -0500

    added script to run web version of tests

--- test/web/mustang.web.test.js
+++ test/web/mustang.web.test.js
@@ -3,11 +3,11 @@ const MustangPage = require("../../pageobjects/mustang.page");
 describe("Mustang POC", () => {
 
     beforeAll(async function () {
-        let contexts = await driver.getContexts();
-        await driver.switchContext(contexts[1]);
+        browser.url('/')
     });
 
     it("can click calendar icon", async () => {
         await MustangPage.clickCalendar(true);
+
     });
 });
