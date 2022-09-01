0bf2a370ecaff3c9ffc47c949b05d7ffd3e2b84f
citizenchris100 <103048026+citizenchris100@users.noreply.github.com>
2022-09-01 09:53:13 -0500

    updated mobile to run sequentially

--- wdio.mobile.conf.js
+++ wdio.mobile.conf.js
@@ -34,7 +34,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test/specs/**/*.js"],
+  specs: [["./test/specs/**/*.js"]],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
@@ -55,7 +55,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  // maxInstances: 10,
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
